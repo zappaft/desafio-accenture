26205c8e1edd5018deb786fe82991e7acb4a53e0
nilojr90 <nilojr90@gmail.com>
2020-10-17 18:42:59 -0300

    fix: requisição ao back end

--- frontEnd/code.js
+++ frontEnd/code.js
@@ -13,25 +13,15 @@ function digitar(valor) {
 }
 
 async function ok() {
-    /* 
-        let http = new XMLHttpRequest();
-    
-        http.open("GET", traducaoURL + inputCode.value, true);
-        http.send(null);
-        console.log(http.response);
-        tela.innerHTML =
-            tela.innerHTML + `<p class='texto'>${http.response}</p>`;
-        //resultado.innerHTML = "<p class='texto'>ACCENTURE</p>";
-     */
 
     console.log("response");
     await fetch(traducaoURL + inputCode.value, {
         method: 'GET',
         mode: 'cors',
     }).then(response => {
-        console.log(response);
+        console.log(response.body);
         tela.innerHTML =
-            tela.innerHTML + `<p class='texto'>${response}</p>`;
+            tela.innerHTML + `<p class='texto'>${response.body}</p>`;
     })
 }
 
