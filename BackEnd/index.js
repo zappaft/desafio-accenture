1a33785adac477d81c660c2abc75a6558d2fec73
nilojr90 <nilojr90@gmail.com>
2020-10-17 18:32:13 -0300

    wip: ativando cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { request } = require('express');
 
-
+const cors = require('cors');
 
 const app = express();
 
--- frontEnd/code.js
+++ frontEnd/code.js
@@ -3,21 +3,36 @@ let tela = document.getElementById("box-title");
 let resultado = document.getElementById("resultado");
 let telefone = document.getElementById("tijolao");
 let bgTijolao = document.getElementById("bgTijolao");
+
+const traducaoURL = "http://localhost:3000/traduzir/";
+
 function digitar(valor) {
 
     inputCode.value += valor;
 
 }
 
-function ok() {
+async function ok() {
+    /* 
+        let http = new XMLHttpRequest();
+    
+        http.open("GET", traducaoURL + inputCode.value, true);
+        http.send(null);
+        console.log(http.response);
+        tela.innerHTML =
+            tela.innerHTML + `<p class='texto'>${http.response}</p>`;
+        //resultado.innerHTML = "<p class='texto'>ACCENTURE</p>";
+     */
 
-    let http = new XMLHttpRequest();
-
-    http.open("GET", traducaoURL + inputCode.value, true);
-    http.send(null);
-    console.log(http.response);
-    tela.innerHTML = tela.innerHTML + `<p class='texto'>${http.response}</p>`;
-    //resultado.innerHTML = "<p class='texto'>ACCENTURE</p>";
+    console.log("response");
+    await fetch(traducaoURL + inputCode.value, {
+        method: 'GET',
+        mode: 'cors',
+    }).then(response => {
+        console.log(response);
+        tela.innerHTML =
+            tela.innerHTML + `<p class='texto'>${response}</p>`;
+    })
 }
 
 
